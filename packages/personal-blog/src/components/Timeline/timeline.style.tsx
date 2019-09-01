81545a0f7162887aee58cdc2077a5ddf5f15b51d
frivolta <rivoltafilippo@gmail.com>
2019-09-01 21:53:52 +0200

    :bug: Fixing graphqlbug

---
+++ packages/personal-blog/src/components/Timeline/timeline.style.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+import { themeGet } from "styled-system"
+
+export const TimelineWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex: 1 1 25%;
+  justify-content: space-between;
+  @media (max-width: 990px) {
+  }
+  @media (max-width: 575px) {
+  }
+`
+export const TimelineItem = styled.div`
+  background-color: ${themeGet("colors.primary")};
+  flex: 1 1 0;
+  text-align: center;
+  padding-top: 48px;
+  padding-bottom: 48px;
+  margin: 0 8px 0;
+  box-shadow: 5px 17px 51px -13px rgba(0,0,0,.57);
+  font-size: 14px;
+  color: #ffffff;
+  font-family: "Poppins", sans-serif;
+  opacity: 0.68;
+
+  &:first-child{
+    margin-left:0;
+  }
+ 
+  &:last-child{
+    margin-right:0;
+  }
+  
+  &.active{
+    opacity: 1;
+  }
+}`
